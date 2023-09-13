5c8dbfa2742c4b282b4afc76983abea5935fdd34
doronz88 <doron88@gmail.com>
2023-09-13 14:17:12 +0300

    install_linux: update apt list

--- install_linux.py
+++ install_linux.py
@@ -70,7 +70,7 @@ def install_packages():
 
     apt_packages = ['git', 'git-lfs', 'cmake', 'openssl', 'bat', 'fzf', 'wget', 'htop', 'curl', 'ncdu', 'watch',
                     'bash-completion', 'ripgrep', 'python-tk', 'nodejs', 'jq', 'tldr', 'vim', 'sublime-text',
-                    'wireshark', 'terminator']
+                    'wireshark', 'terminator', 'net-tools', 'build-essential']
 
     snap_packages = ['pycharm-community', 'code']
 
